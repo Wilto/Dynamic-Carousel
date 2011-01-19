1c36be6403f489d698e3595caba347eaada7e52c
Mat Marquis <mat@matmarquis.com>
2011-01-19 08:58:33 -0800

    Surprise! I broke things. Should be fixed now.

--- plugin.js
+++ plugin.js
@@ -17,46 +17,43 @@
 
 		$(opt.prevSlide).addClass('disabled');
 
-		function move($slider, direction) {
+		function moveNext($slider) {
 			var leftmargin = $slider.attr('style').match(/margin\-left: ?(.*)%;/) && parseFloat(RegExp.$1),
 				$slide = $slider.find(opt.slide);
 
-			if(!$slider.is(":animated")) {	
-				if(direction == 'next') {
-					leftmargin -= 100;
-				} else {
-					leftmargin += 100;
-				}
-				
+			if (!$slider.is(":animated") && (-leftmargin) != (($slide.length - 1) * 100)) {
+				leftmargin -= 100;
 				if(transitionSupport) {
 					$slider.css('marginLeft', leftmargin + "%");
 				} else {
 					$slider.animate({ marginLeft: leftmargin + "%" }, opt.speed);
 				}
-								
-				if((-leftmargin) == ($slide.length - 1) * 100 || leftmargin == 0) {
+				if((-leftmargin) == ($slide.length - 1) * 100) {
 					return false;
 				}
 			}
 		}
 
+		function movePrev($slider) {
+			var leftmargin = $slider.attr('style').match(/margin\-left: ?(.*)%;/) && parseFloat(RegExp.$1);
+
+			if(!$slider.is(":animated") && (leftmargin != 0)) {
+				leftmargin += 100;
+				if(transitionSupport) {
+					$slider.css('marginLeft', leftmargin + "%");
+				} else {
+					$slider.animate({ marginLeft: leftmargin + "%" }, opt.speed);
+				}					
+				if(leftmargin == 0) {
+					return false;
+				}
+			}
+		}
 
 		$(opt.nextSlide + ',' + opt.prevSlide).click(function(e) {
 			var $el = $(this),
 				link = $el.attr('href'),
 				$target = $(opt.slider).filter(link);
-		
-				if(!$el.hasClass('disabled')) {
-					$(opt.nextSlide).each(function() {
-						 if(this == $el[0]) {
-							if(move($target, 'next') === false) {
-								$el.addClass('disabled');
-							};
-							$(opt.prevSlide).filter(function() { 
-								return this.getAttribute('href') === link;
-							}).removeClass('disabled');
-						}
-					});
 
 				$(opt.nextSlide).each(function() {
 					if($(this)[0] == $el[0]) {
@@ -79,9 +76,10 @@
 						}).removeClass('disabled');
 					}
 				});
-			}
-		        e.preventDefault();
+
+			e.preventDefault();
 		});
+
 
 		//swipes trigger move left/right
 		$( this ).live( "swipe", function( e, ui ){
