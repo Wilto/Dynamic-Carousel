d8fa2a5a5b6bbbcfa0a59be84bbd5a38cf031064
Mat Marquis <mat@matmarquis.com>
2011-01-23 17:22:00 -0500

    Fixed up the regexes for Opera/IE.

--- plugin.js
+++ plugin.js
@@ -8,65 +8,60 @@
 			speed: 500
 		},
 		opt = $.extend(defaults, config),
-<<<<<<< HEAD
 		dStyle = document.body.style,
 		transitionSupport = dStyle.webkitTransition !== undefined || 
 				    dStyle.MozTransition !== undefined ||
 				    dStyle.msTransition !== undefined ||
 				    dStyle.OTransition !== undefined ||
 				    dStyle.transition !== undefined;
-=======
-		transitionSupport = document.body.style.webkitTransition !== undefined || 
-				    document.body.style.MozTransition !== undefined ||
-				    document.body.style.msTransition !== undefined ||
-				    document.body.style.OTransition !== undefined ||
-				    document.body.style.transition !== undefined;
->>>>>>> 913912769e471e4280d0278f551143065d59bc76
 
 		$(opt.prevSlide).addClass('disabled');
 
-		function move($slider, direction) {
-			var leftmargin = $slider.attr('style').match(/margin\-left: ?(.*)%;/) && parseFloat(RegExp.$1),
+		function moveNext($slider) {
+			var leftmargin = $slider.attr('style').match(/margin\-left:(.*[0-9])/i) && parseInt(RegExp.$1),
 				$slide = $slider.find(opt.slide);
-
-			if(!$slider.is(":animated")) {	
-				if(direction == 'next') {
-					leftmargin -= 100;
-				} else {
-					leftmargin += 100;
-				}
+			
+			if (!$slider.is(":animated") && (-leftmargin) != (($slide.length - 1) * 100)) {
+				leftmargin -= 100;
+				
+				alert(leftmargin);
+				alert($slider.attr('style'));
 				
 				if(transitionSupport) {
 					$slider.css('marginLeft', leftmargin + "%");
 				} else {
 					$slider.animate({ marginLeft: leftmargin + "%" }, opt.speed);
 				}
-								
-				if((-leftmargin) == ($slide.length - 1) * 100 || leftmargin == 0) {
+				if((-leftmargin) == ($slide.length - 1) * 100) {
 					return false;
 				}
 			}
 		}
 
+		function movePrev($slider) {
+			var leftmargin = $slider.attr('style').match(/margin\-left:(.*[0-9])/i) && parseInt(RegExp.$1);
+
+			if(!$slider.is(":animated") && (leftmargin != 0)) {
+				leftmargin += 100;
+
+				alert(leftmargin);
+				alert($slider.attr('style'));
+
+				if(transitionSupport) {
+					$slider.css('marginLeft', leftmargin + "%");
+				} else {
+					$slider.animate({ marginLeft: leftmargin + "%" }, opt.speed);
+				}					
+				if(leftmargin == 0) {
+					return false;
+				}
+			}
+		}
 
 		$(opt.nextSlide + ',' + opt.prevSlide).click(function(e) {
 			var $el = $(this),
 				link = $el.attr('href'),
 				$target = $(opt.slider).filter(link);
-<<<<<<< HEAD
-				
-				if(!$el.hasClass('disabled')) {
-					$(opt.nextSlide).each(function() {
-						 if(this == $el[0]) {
-							if(move($target, 'next') === false) {
-								$el.addClass('disabled');
-							};
-							$(opt.prevSlide).filter(function() { 
-								return this.getAttribute('href') === link;
-							}).removeClass('disabled');
-						}
-					});
-=======
 
 				$(opt.nextSlide).each(function() {
 					if($(this)[0] == $el[0]) {
@@ -89,25 +84,14 @@
 						}).removeClass('disabled');
 					}
 				});
->>>>>>> 913912769e471e4280d0278f551143065d59bc76
 
-					$(opt.prevSlide).each(function() {
-						 if(this == $el[0]) {
-							if(move($target, 'prev') === false) {
-								$el.addClass('disabled');
-							};
-							$(opt.nextSlide).filter(function() {
-								return this.getAttribute('href') === link;
-							}).removeClass('disabled');
-						}
-					});
-				}
 			e.preventDefault();
 		});
 
+
 		//swipes trigger move left/right
-		$( this ).live( "swipe", function( e, ui ){
-			( ui.direction === "left" ? moveNext : movePrev )( $(this).find( opt.slider ) );
+		$(this).live( "swipe", function(e, ui){
+			(ui.direction === "left" ? moveNext : movePrev)($(this).find( opt.slider ));
 		});
 
 		return this.each(function() {
@@ -118,19 +102,21 @@
 				speed = opt.speed / 1000;
 
 			$wrap.css({
-				float: "left",
-				overflow: "hidden"
+				overflow: "hidden",
+				width: '100%'
 			});
+			
 			$slider.css({
 				marginLeft: "0px",
-				styleFloat: "left",
+				float: "left",
 				width: 100 * slidenum + "%",
 				"-webkit-transition": "margin-left " + speed + "s ease",
 				"-moz-transition": "margin-left " + speed + "s ease",
 				"-ms-transition": "margin-left " + speed + "s ease",
 				"-o-transition": "margin-left " + speed + "s ease",
 				"transition": "margin-left " + speed + "s ease"
-			});		    
+			});	
+				    
 			$slide.css({
 				float: "left",
 				width: (100 / slidenum) + "%"				
@@ -143,52 +129,47 @@
 	// also handles swipeleft, swiperight
 	$.event.special.swipe = {
 		setup: function() {
-			var thisObject = this,
-				$this = $( thisObject );
+			var $el = $(this);
 			
-			$this
-				.bind( "touchstart", function( event ) {
-					var data = event.originalEvent.touches ?
-							event.originalEvent.touches[ 0 ] :
-							event,
+			$el
+				.bind("touchstart", function(e) {
+					var data = e.originalEvent.touches ? e.originalEvent.touches[0] : e,
 						start = {
 							time: (new Date).getTime(),
 							coords: [ data.pageX, data.pageY ],
-							origin: $( event.target )
+							origin: $(e.target)
 						},
 						stop;
 					
-					function moveHandler( event ) {
-						if ( !start ) {
+					function moveHandler(e) {
+						if(!start) {
 							return;
 						}
 						
-						var data = event.originalEvent.touches ?
-								event.originalEvent.touches[ 0 ] :
-								event;
+						var data = e.originalEvent.touches ? e.originalEvent.touches[0] : e;
 						stop = {
 								time: (new Date).getTime(),
 								coords: [ data.pageX, data.pageY ]
 						};
 						
 						// prevent scrolling
-						if ( Math.abs( start.coords[0] - stop.coords[0] ) > 10 ) {
-							event.preventDefault();
+						if (Math.abs(start.coords[0] - stop.coords[0]) > 10) {
+							e.preventDefault();
 						}
 					}
 					
-					$this
-						.bind( "touchmove", moveHandler )
-						.one( "touchend", function( event ) {
-							$this.unbind( "touchmove", moveHandler );
-							if ( start && stop ) {
-								if ( stop.time - start.time < 1000 && 
-										Math.abs( start.coords[0] - stop.coords[0]) > 30 &&
-										Math.abs( start.coords[1] - stop.coords[1]) < 75 ) {
+					$el
+						.bind("touchmove", moveHandler)
+						.one("touchend", function(e) {
+							$el.unbind("touchmove", moveHandler);
+							if (start && stop) {
+								if (stop.time - start.time < 1000 && 
+										Math.abs(start.coords[0] - stop.coords[0]) > 30 &&
+										Math.abs(start.coords[1] - stop.coords[1]) < 75) {
 										var left = start.coords[0] > stop.coords[0];
 									start.origin
-									.trigger( "swipe", {direction: left ? "left" : "right"} )
-									.trigger( left ? "swipeleft" : "swiperight" );
+										.trigger("swipe", {direction: left ? "left" : "right"})
+										.trigger(left ? "swipeleft" : "swiperight" );
 								}
 							}
 							start = stop = undefined;
