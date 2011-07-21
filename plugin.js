181cbe213d5e8f633d77600d79103871c5089392
Mat Marquis <mat@matmarquis.com>
2011-07-21 11:55:30 -0400

    It just got real (see associated heading in readme.md).

--- plugin.js
+++ plugin.js
@@ -1,123 +1,353 @@
 /*! (c) Mat Marquis (@wilto). MIT License. http://wil.to/3a */
 
-(function($){
+(function( $, undefined ) {
+	var inst = 0;
+
 	$.fn.carousel = function(config) {
+		var defaults = {
+			slider			: '.slider',
+			slide			: '.slide',
+			prevSlide		: null,
+			nextSlide		: null,
+			slideHed		: null,
+			addPagination	: false,
+			addNav			: (config !== undefined && (config.prevSlide || config.nextSlide)) ? false : true,
+			namespace		: 'carousel',
+			speed			: 300
+		},
+		$slidewrap = this,
+		opt = $.extend(defaults, config),
 		carousel = {
+			init : function() {
+				inst++;
+								
+				$slidewrap.each(function(carInt) {
+						var $wrap = $(this),
+							$slider = $wrap.find(opt.slider),
+							$slide = $wrap.find(opt.slide),			
+							slidenum = $slide.length,
+							transition = "margin-left " + ( opt.speed / 1000 ) + "s ease",
+							tmp = 'carousel-' + inst + '-' + carInt;
+
+						$wrap
+							.css({
+								overflow: "hidden",
+								width: "100%"
+							})
+							.attr('role', 'application');
+
+						$slider.css({
+							marginLeft: "0px",
+							float: "left",
+							width: 100 * slidenum + "%",
+							"-webkit-transition": transition,
+							"-moz-transition": transition,
+							"-ms-transition": transition,
+							"-o-transition": transition,
+							"transition": transition
+						});
+
+						$slide
+							.css({
+								float: "left",
+								width: (100 / slidenum) + "%"				
+							})
+							.each(function(i) {
+								var $el = $(this);
+
+								$el.attr({
+									role : "tabpanel document",
+									id : tmp + '-slide' + i
+								});
+
+								if( opt.addPagination ) {
+									$el.attr('aria-labelledby', tmp + '-tab' + i);
+								}
+							});
+					
+					if( !$slider[0].id ) {
+						$slider.attr('id', 'carousel-' + inst + '-' + carInt);
+					}
+										
+					// Build and insert navigation/pagination, if specified in the options.
+					opt.addPagination 	&& carousel.addPagination();
+					opt.addNav 			&& carousel.addNav();
+					
+					carousel.navState($slider, 0);
+				});
+			},
+			addNav : function() {
+				$slidewrap.each(function(i) {						
+					var $oEl = $(this),
+						currentSlider = $oEl.find(opt.slider)[0].id,
+						navMarkup = [
+							'<ul class="slidecontrols" role="navigation">',
+							'	<li role="presentation"><a href="#' + currentSlider + '" class="' + opt.namespace + '-next">Next</a></li>',
+							'	<li role="presentation"><a href="#' + currentSlider + '" class="' + opt.namespace + '-prev">Prev</a></li>',
+							'</ul>'
+							].join('');
+
+					opt.nextSlide = '.' + opt.namespace + '-next';
+					opt.prevSlide = '.' + opt.namespace + '-prev';
+
+					$oEl.prepend(navMarkup);
+				});
+			},
+			addPagination : function() {
+				$slidewrap.each(function(i) {
+					var $oEl = $(this),
+						$pagination = $('<ol class="' + opt.namespace + '-tabs" role="tablist navigation" />'),
+						slides = $oEl.find(opt.slide).length,
+						associated = 'carousel-' + inst + '-' + i;
+						
+					while( slides-- ) {
+						var i = slides,
+							hed = $($oEl.find(opt.slide)[i]).find(opt.slideHed).text() || 'Page ' + ( i + 1 ),
+							tabMarkup = [
+								'<li role="presentation">',
+								'	<a href="#' + associated + '-slide' + i +'"',
+								' aria-controls="' + associated + '-slide' + i +'"',
+								' id="' + associated + '-tab' + i + '" role="tab">',
+								 hed + '</a>',
+								'</li>'
+							].join('');
+						
+						$pagination.prepend(tabMarkup);
+					};
+
+					$pagination
+						.appendTo($oEl)
+						.find("li").keydown(function(e) {
+							var $el = $(this),
+								$prevTab = $el.prev().find('a'),
+								$nextTab = $el.next().find('a');
+
+							switch (e.which) {
+								case 37:
+								case 38:		
+									$prevTab.length && $prevTab.trigger('click').focus();
+									e.preventDefault();
+									break;
+								case 39: 
+								case 40:
+									$nextTab.length && $nextTab.trigger('click').focus();
+									e.preventDefault();
+									break;
+							}
+						}).find('a').click(function(e) {
+							var $el = $(this)
+								$tab = $el.parent(),
+								current = $tab.index(),
+								move = -(100 * (current)),
+								$slider = $oEl.find(opt.slider);
+
+							carousel.move($slider, move);
+
+							e.preventDefault();
+						});
+				});
+			},
 			roundDown : function(leftmargin) {
 				var leftmargin = parseInt(leftmargin, 10);
-				
+
 				return Math.ceil( (leftmargin - (leftmargin % 100 ) ) / 100) * 100;
 			},
-			transitionSupport : function() {
-				var dStyle = document.body.style;
+			navState : function($slider, moveTo) {
+				var $target = $( '[href="#' + $slider.attr('id') + '"]');
+					$slides = $slider.find(opt.slide),
+					ind = -(moveTo / 100),
+					activeSlide = $slides[ind];
+					
+				$slider.attr('aria-activedescendant', activeSlide.id);
 				
-				return dStyle.webkitTransition !== undefined || 
-						dStyle.mozTransition !== undefined ||
-						dStyle.msTransition !== undefined ||
-						dStyle.oTransition !== undefined ||
-						dStyle.transition !== undefined;
+				// Update state of active tabpanel
+				$(activeSlide)
+					.addClass(opt.namespace + "-active-slide")
+					.attr('aria-hidden', false)
+						.find('*')
+						.removeAttr('tabindex')
+					.end()
+					.siblings()	
+						.removeClass(opt.namespace + "-active-slide")
+						.attr('aria-hidden', true)
+							.find('*')
+							.attr('tabindex', -1);
+
+				// Update state of next/prev navigation
+				if( opt.addNav || (opt.prevSlide || opt.nextSlide) ) {
+					$target.removeClass(opt.namespace + '-disabled');
+					switch( moveTo ) {
+						case ( -($slides.length - 1) * 100 ):
+							$target.filter(opt.nextSlide).addClass(opt.namespace + '-disabled');
+							break;
+						case 0:
+							$target.filter(opt.prevSlide).addClass(opt.namespace + '-disabled');
+							break;
+					}
+				}
+				
+				// Update state of pagination tabs.
+				if( opt.addPagination ) {
+					var tabId = $(activeSlide).attr('aria-labelledby'),
+						$tab = $('#' + tabId );
+						
+					$tab
+						.parent()
+						.addClass(opt.namespace + '-active-tab')
+						.siblings()
+						.removeClass(opt.namespace + '-active-tab')
+						.find('a')
+							.attr({
+								'aria-selected' : false,
+								'tabindex' : -1
+							});
+							
+					$tab.attr({
+						'aria-selected' : true,
+						'tabindex' : 0
+					});
+				}
 			},
 			move : function($slider, moveTo) {
-				if(carousel.transitionSupport()) {
+				var $slides = $slider.find(opt.slide),
+					current = -(moveTo / 100),
+					$activeSlide = $($slides[current]),
+					activeId = $slides[current].id,
+					dStyle = document.body.style,
+					transitionSupport = dStyle.webkitTransition !== undefined || 
+										dStyle.mozTransition !== undefined ||
+										dStyle.msTransition !== undefined ||
+										dStyle.oTransition !== undefined ||
+										dStyle.transition !== undefined;
+					
+				if( transitionSupport ) {
 					$slider.css('marginLeft', moveTo + "%");
 				} else {
 					$slider.animate({ marginLeft: moveTo + "%" }, opt.speed);
 				}
-			}
-		};
-		var defaults = {
-			slider: '.slider',
-			slide: '.slide',
-			prevSlide: '.prev',
-			nextSlide: '.next',
-			speed: 500
-		},
-		opt = $.extend(defaults, config),
-		nextPrev = function($slider, dir) {
-			var leftmargin = ( $slider ) ? $slider.attr('style').match(/margin\-left:(.*[0-9])/i) && parseInt(RegExp.$1) : 0,
-				$slide = $slider.find(opt.slide),
-				constrain = dir === 'prev' ? leftmargin != 0 : -leftmargin < ($slide.length - 1) * 100,
-				$target = $( '[href="#' + $slider.attr('id') + '"]');
-								
-			if (!$slider.is(":animated") && constrain ) {
-								
-				if ( dir === 'prev' ) {
-					leftmargin = ( leftmargin % 100 != 0 ) ? carousel.roundDown(leftmargin) : leftmargin + 100;
+				carousel.navState($slider, moveTo);
+			},
+			nextPrev : function($slider, dir) {
+				var leftmargin = ( $slider ) ? $slider.attr('style').match(/margin\-left:(.*[0-9])/i) && parseInt(RegExp.$1) : 0,
+					$slide = $slider.find(opt.slide),
+					constrain = dir === 'prev' ? leftmargin != 0 : -leftmargin < ($slide.length - 1) * 100,
+					$target = $( '[href="#' + $slider.attr('id') + '"]');
+
+				if (!$slider.is(":animated") && constrain ) {
+
+					if ( dir === 'prev' ) {
+						leftmargin = ( leftmargin % 100 != 0 ) ? carousel.roundDown(leftmargin) : leftmargin + 100;
+					} else {
+						leftmargin = ( ( leftmargin % 100 ) != 0 ) ? carousel.roundDown(leftmargin) - 100 : leftmargin - 100;
+					}
+
+					carousel.move($slider, leftmargin);
+
+					$target
+						.removeClass( opt.namespace + '-disabled')
+						.removeAttr('aria-disabled');
+
+					switch( leftmargin ) {
+						case ( -($slide.length - 1) * 100 ):
+							$target.filter(opt.nextSlide)
+								.addClass( opt.namespace + '-disabled')
+								.attr('aria-disabled', true);
+							break;
+						case 0:
+							$target.filter(opt.prevSlide)
+								.addClass( opt.namespace + '-disabled')
+								.attr('aria-disabled', true);
+							break;
+					}
 				} else {
-					leftmargin = ( ( leftmargin % 100 ) != 0 ) ? carousel.roundDown(leftmargin) - 100 : leftmargin - 100;
-				}
-				
-				carousel.move($slider, leftmargin);
-				$target.removeClass('disabled');
+					var reset = carousel.roundDown(leftmargin);
 
-				switch( leftmargin ) {
-					case ( -($slide.length - 1) * 100 ):
-						$target.filter(opt.nextSlide).addClass('disabled');
-						break;
-					case 0:
-						$target.filter(opt.prevSlide).addClass('disabled');
-						break;
+					carousel.move($slider, reset);
 				}
-			} else {
-				var reset = carousel.roundDown(leftmargin);
-				
-				carousel.move($slider, reset);
 			}
 		};
+	
+		carousel.init(this);
 
-		$(opt.nextSlide + ',' + opt.prevSlide).click(function(e) {
-			var $el = $(this),
-				link = $el.attr('href'),
-				dir = ( $el.is(opt.prevSlide) ) ? 'prev' : 'next',
-				$slider = $(link);
+		$(opt.nextSlide + ',' + opt.prevSlide)
+			.bind('click', function(e) {
+				var $el = $(this),
+					link = $el.attr('href'),
+					dir = ( $el.is(opt.prevSlide) ) ? 'prev' : 'next',
+					$slider = $(link);
 
-				if ( $el.is('.disabled') ) { 
-					return false;
-				}
+					if ( $el.is('.' + opt.namespace + '-disabled') ) { 
+						return false;
+					}
 
-				nextPrev($slider, dir);
+					carousel.nextPrev($slider, dir);
 				
-			e.preventDefault();
-		});
-		$(opt.prevSlide).addClass('disabled');
+				e.preventDefault();
+			})
+			.bind('keydown', function(e) {
+				var $el = $(this),
+					link = this.getAttribute('href');
 
-		//swipes trigger move left/right
-		$(this).live( "dragSnap", function(e, ui){
+				switch (e.which) {
+					case 37:
+					case 38:
+						$(opt.prevSlide).filter('[href="' + link + '"]').trigger('click').focus();
+						e.preventDefault();
+						break;
+					case 39:
+					case 40:
+						$(opt.nextSlide).filter('[href="' + link + '"]').trigger('click').focus();
+						e.preventDefault();
+						break;
+				}
+			});
+
+		$('[data-autorotate]').each(function() {
+			var $el = $(this),
+				speed = this.getAttribute('data-autorotate'),
+				auto,
+				autoAdvance = function() {
+					var slidenum = $el.find(opt.slide).length,
+						active = -(parseInt($(opt.slider).css('marginLeft'), 10) / 100) + 1;
+					
+					switch( active ) {
+						case slidenum: 
+							clearInterval(auto);
+					
+							auto = setInterval(function() {
+								autoAdvance();
+								$el.find(opt.prevSlide).trigger('click');
+							}, speed);
+							
+							break;
+						case 1:
+							clearInterval(auto);
+
+							auto = setInterval(function() {
+								autoAdvance();
+								$el.find(opt.nextSlide).trigger('click');
+							}, speed);
+							
+							break;
+					}
+				};
+
+			auto = setInterval(autoAdvance, speed);
+			
+			$el
+				.attr('aria-live', 'polite')
+				.bind('mouseenter', function() {
+					clearInterval(auto);
+				});
+		});
+
+		this.bind( "dragSnap", function(e, ui){
 			var $slider = $(this).find( opt.slider ),
 				dir = ( ui.direction === "left" ) ? 'next' : 'prev';
-				
-			nextPrev($slider, dir);
-		});
-
-		return this.each(function() {
-			var $wrap = $(this),
-				$slider = $wrap.find(opt.slider),
-				$slide = $wrap.find(opt.slide),			
-				slidenum = $slide.length,
-				transition = "margin-left " + ( opt.speed / 1000 ) + "s ease";
-				
-			$wrap.css({
-				overflow: "hidden",
-				width: "100%"
-			});
 			
-			$slider.css({
-				marginLeft: "0px",
-				float: "left",
-				width: 100 * slidenum + "%",
-				"-webkit-transition": transition,
-				"-moz-transition": transition,
-				"-ms-transition": transition,
-				"-o-transition": transition,
-				"transition": transition
-			});
-				    
-			$slide.css({
-				float: "left",
-				width: (100 / slidenum) + "%"				
-			});
-
+			carousel.nextPrev($slider, dir);
 		});
+
 	};
 })(jQuery);
 
@@ -137,27 +367,29 @@ $.event.special.dragSnap = {
 					"transition": transition
 				});
 			},
-			move = function($slider, moveTo) {
-				var dStyle = document.body.style,
-				 	transitionSupport = dStyle.webkitTransition !== undefined || 
+			roundDown = function(left) {
+				var leftmargin = parseInt(left, 10);
+				
+				return Math.ceil( (left - (left % 100 ) ) / 100) * 100;
+			},
+			snapBack = function(e, ui) {
+				var $el = ui.target,
+					currentPos = ( $el.attr('style') != undefined ) ? $el.attr('style').match(/margin\-left:(.*[0-9])/i) && parseInt(RegExp.$1) : 0,
+					leftmargin = (ui.left === false) ? roundDown(currentPos) - 100 : roundDown(currentPos),
+					dStyle = document.body.style,
+					transitionSupport = dStyle.webkitTransition !== undefined || 
 						dStyle.mozTransition !== undefined ||
 						dStyle.msTransition !== undefined ||
 						dStyle.oTransition !== undefined ||
 						dStyle.transition !== undefined;
 
-				if( transitionSupport ) {
-					$slider.css('marginLeft', moveTo + "%");
-				} else {
-					$slider.animate({ marginLeft: moveTo + "%" }, opt.speed);
-				}
-			},
-			snapBack = function(e, ui) {
-				var $el = ui.target,
-					currentPos = ( $el.attr('style') != undefined ) ? $el.attr('style').match(/margin\-left:(.*[0-9])/i) && parseInt(RegExp.$1) : 0,
-					leftmargin = (ui.left === false) ? carousel.roundDown(currentPos) - 100 : carousel.roundDown(currentPos);
-
 				transitionSwap($el, true);
-				move($el, leftmargin);	
+				
+				if( transitionSupport ) {
+					$el.css('marginLeft', leftmargin + "%");
+				} else {
+					$el.animate({ marginLeft: leftmargin + "%" }, opt.speed);
+				}
 			};
 
 		$el
--- readme.md
+++ readme.md
@@ -1,8 +1,31 @@
 # A Responsive Carousel
 If there's one thing that can compete with lightboxes for “world’s most done-to-death jQuery plugin,” it’s carousels. However, everything I came across was using pixel values and not percentages—meaning I wasn’t finding much of anything that could be used on a responsive/flexible layout. So I built one.
 
-## Aw yeah.
-The new dragSnap event has been merged into the main branch—it addresses a bunch of issues, and works pretty damn well if I do say so.
+## It Just Got Real
+We got pagination, we got generated navigation, we got keyboard support, and we got accessibility for days.
+
+### Pagination
+You’ll notice there’s now an ```addPagination``` option. This loops through the slides and generates a list of fully—ARIA’d links to each slide, complete with keyboard support (arrow keys). There’s an associated ```slideHed``` option, which you’ll see in play on the first carousel—this allows you to use the value of an element within the slide as the text in the associated link. If none is specified, you get “Page 1,” “Page 2,” and so on, but that isn’t very helpful to a screen reader. Definitely think about using the ```slideHed``` option—even if you’re planning on hiding the text using ```text-indent``` or somesuch. Even in-page links should be meaningful.
+
+I styled one of the examples, for the hell of it. You are free to steal this.
+
+### Generated Next/Prev Links
+You can do that now, using ```addNav```—this way, non-JS users don’t have mysterious, useless links floating around in their markup. You can, of course, still write them into the markup yourself (see the second example carousel).
+
+### Auto-advance
+This exists now. I was very hesitant, because I don’t generally like the idea of things flying around on the page without the user specifically requesting it. Currently it’s triggering a click event on the next/prev links, which means that this will only work if you’ve added them to the markup or opted for the script to generate them. This won’t be a requirement on the next release, but for now just hide ’em if the situation calls for it.
+
+The auto-rotation stops on mouseover, as we can assume the user is interacting with the content at that point.
+
+### Keyboard Support
+Arrow keys, when focus is on the next/prev links or the pagination tabs.
+
+### Accessibility
+Needs another pass in JAWS, but it’s a lot more usable from a screenreader perspective. You need not do a thing.
+It does this for you. 
+
+## It’s Gonna Get Realer
+Improvements to the dragSnap event are coming down the Pike, thanks to one Mr. @scottjehl.
 
 ## “Dynamic?” Isn’t that a little “DHTML for Dummies?”
-Well, it’s not static, is it? I’m takin’ it back.
+I’m takin’ it back.
